72d8e1f70f572abdea370463e44ee31c8383ddc8
Mike Møller Nielsen <mmni@protonmail.com>
2022-06-11 23:08:11 +0200

    More files

--- src/main/java/com/example/springbootapachecamel/SpringbootApacheCamelApplication.java
+++ src/main/java/com/example/springbootapachecamel/SpringbootApacheCamelApplication.java
@@ -3,7 +3,10 @@ package com.example.springbootapachecamel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringbootApacheCamelApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/springbootapachecamel/integration/MyKafkaRouteBuilder.java
@@ -0,0 +1,20 @@
+package com.example.springbootapachecamel.integration;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyKafkaRouteBuilder extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer:mikestimer?delay=2000&period=4000&repeatCount=5")
+                .setBody((exchange -> {
+                    String myheadersin = exchange.getIn().getHeaders().toString();
+                    return "Hello... " + LocalDateTime.now() + ", headers: " + myheadersin;
+                }))
+                .to("log:mikeslog")
+                .to("file:output");
+    }
+}
